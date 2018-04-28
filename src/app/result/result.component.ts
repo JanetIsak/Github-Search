a3c9860abc410de68ed358b1fc92ff1efc187e05
Janet Isak <janetisak@yahoo.com>
2018-04-28 17:59:09 +0300

    creat User and Repository classes

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-result',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+  githubUser:string
+  apiKey:string="25f521bc0a2ee910ef9b1f03889f3f4ad50da2b3"
+  userLink:string="https://api.github.com/users/"
+  link:string
+  userdata:string
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
-
+  searchUser(){
+    this.link=this.userLink+this.githubUser+"?access_token="+this.apiKey
+    this.http.get(this.link).subscribe((response:any)=>{
+      this.userdata=response
+      console.log(this.userdata)
 }
